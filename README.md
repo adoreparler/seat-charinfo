0aed320d31dbba83b9b4b9f4d693673686244ade
seion <seion@sirius.sirius.seion.life>
2025-11-17 00:32:45 +0000

    Removing a override of a final function

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-TBD
+A single page plugin that gives more information on users in once view
--- src/CharinfoServiceProvider.php
+++ src/CharinfoServiceProvider.php
@@ -9,7 +9,7 @@ class CharinfoServiceProvider extends AbstractSeatPlugin
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/Http/routes.php');
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'charinfo');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'charinfo');
         $this->mergeConfigFrom(__DIR__ . '/Config/package.character.menu.php', 'package.character.menu');
     }
 
@@ -20,7 +20,6 @@ class CharinfoServiceProvider extends AbstractSeatPlugin
 
     public function getName(): string { return 'Character Info'; }
     public function getPackage(): string { return 'seat-charinfo'; }
-    public function getVersion(): string { return '1.0.0'; }
     public function getAuthor(): string { return 'Adoreparler'; }
     public function getIcon(): string { return 'fas fa-info-circle'; }
     public function getUrl(): ?string { return 'https://github.com/adoreparler/seat-charinfo'; }
